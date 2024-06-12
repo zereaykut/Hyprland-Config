19fe229ca2976322b579035246d6b00776ab0893
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-06-12 23:56:43 +0300

    Extract Themes Icons Cursors

---
+++ 07 - get_themes_icons_cursors.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#------------#
+# GTK Themes #
+#------------#
+tar -xzvf .themes/Gtk_CatppuccinLatte.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_CatppuccinMocha.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_CyberpunkEdge.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_DecayGreen.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_Dracula.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_FrostedGlass.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_GraphiteMono.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_GruvboxRetro.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_MaterialSakura.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_NordicBlue.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_RosePine.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_SweetMars.tar.gz -C ~/.themes
+tar -xzvf .themes/Gtk_SynthWave.tar.gz -C ~/.themes
+
+#-------------#
+# Icon Themes #
+#-------------#
+tar -xzvf .icons/Cursor_Bibata-Modern-Classic.tar.gz -C ~/.icons
+tar -xzvf .icons/Cursor_BibataIce.tar.gz -C ~/.icons
+tar -xzvf .icons/Cursor_Dracula-cursors.tar.gz -C ~/.icons
+tar -xzvf .icons/Cursor_Gruvbox.tar.gz -C ~/.icons
+tar -xzvf .icons/Cursor_Sweet.tar.gz -C ~/.icons
+
+#---------------#
+# Cursor Themes #
+#---------------#
+tar -xzvf .icons/Icon_candy-icons.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_Gruvbox.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_NordicBlue.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_SynthWave.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaBlack.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaBlue.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaDracula.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaGreen.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaGrey.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaPink.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaPurple.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_TelaYellow.tar.gz -C ~/.icons
+tar -xzvf .icons/Icon_Tela-red.tar.gz -C ~/.icons
