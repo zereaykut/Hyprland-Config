2e9aeb64a134e5aff4e18f10fd397bd59bc1cb18
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-07-04 10:06:08 +0300

    Update 05 - enable_systemctl_&_setups.sh
    removed swww cache creation

--- 05 - enable_systemctl_&_setups.sh
+++ 05 - enable_systemctl_&_setups.sh
@@ -24,7 +24,6 @@ sudo systemctl enable firewalld.service
 # Create Folders #
 #----------------#
 mkdir ~/.themes ~/.icons ~/Projects ~/Documents ~/Downloads ~/Videos
-mkdir -p ~/.cache/swww
 mkdir -p ~/Pictures/Screenshots
 mkdir -p ~/Torrent/{torrents,torrent_files,finished_torrents/{Movie,TV,Music,TT},finished_torrent_files,watched_torrent_files}
 
