7ead9a20f2fc003758d9ea3808f99664da5146e1
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-07-09 15:23:35 +0300

    Add files via upload

--- .config/hypr/scripts/brightness_control.py
+++ .config/hypr/scripts/brightness_control.py
@@ -1,40 +1,45 @@
-#!/usr/bin/python
 import argparse
 import subprocess as sp
 import sys
 
-# %% Script statement
-"""
+class BrightnessControl:
+    def __init__(self, action):
+        self.user = sp.run("whoami", shell=True, capture_output=True, text=True).stdout.strip()
+        self.action = action
+
+    def set_brightness(self):
+        if self.action == "i":
+            sp.run("brightnessctl set +5%", shell=True)
+        elif self.action == "d":
+            sp.run("brightnessctl set 5%-", shell=True)
+        else:
+            sys.exit("Invalid action parameter")
+
+    def send_notification(self):
+        brightness_val = int(sp.run("brightnessctl get", shell=True, capture_output=True, text=True).stdout.strip())
+        brightness_max = int(sp.run("brightnessctl max", shell=True, capture_output=True, text=True).stdout.strip())
+        brightness_percent = int(brightness_val / brightness_max * 100)
+        brightness_vol = brightness_percent // 5 * 5
+        icon = f"/home/{self.user}/.config/dunst/icons/vol/vol-{brightness_vol}.svg"
+
+        sp.run(f"""dunstify "Brightness {brightness_percent}%" -i {icon} -r 91190 -t 800 -u normal""", shell=True)
+
+def main():
+    """
     python brightness_control.py --action <parameter>
         << --action >>
             i: increase brightness by 5%
             d: decrease brightness by 5%
-"""
+    """
+    
+    parser = argparse.ArgumentParser(description="Control brightness and send notifications.")
+    parser.add_argument("-a", "--action", help="Action to increase or decrease brightness by 5%", required=True)
 
-#%% User
-user = sp.run("whoami", shell=True, capture_output=True, text=True,).stdout.strip()
+    args = parser.parse_args()
 
-# %% Argument
-parser = argparse.ArgumentParser()
-parser.add_argument("-a", "--action", help="input to increase or decrease brightness by 5%")
+    brightness_control = BrightnessControl(args.action)
+    brightness_control.set_brightness()
+    brightness_control.send_notification()
 
-args = parser.parse_args()
-action = args.action
-
-#%% Set Brightness
-if action == "i":
-    sp.run("brightnessctl set +5%", shell=True)
-elif action == "d":
-    sp.run("brightnessctl set 5%-", shell=True)
-else:
-    sys.exit()
-
-#%% Send Notification
-brightness_val = int(sp.run("brightnessctl get", shell=True, capture_output=True, text=True,).stdout.strip())
-brightness_max = int(sp.run("brightnessctl max", shell=True, capture_output=True, text=True,).stdout.strip())
-brightness_percent = int(brightness_val/brightness_max * 100)
-brightness_vol = brightness_percent//5*5
-icon = f"/home/{user}/.config/dunst/icons/vol/vol-{brightness_vol}.svg"
-
-sp.run(f"""dunstify "Brightness {brightness_percent}%" -i {icon} -r 91190 -t 800 -u normal""", shell=True)
-
+if __name__ == "__main__":
+    main()
--- .config/hypr/scripts/wlogout.py
+++ .config/hypr/scripts/wlogout.py
@@ -1,35 +1,47 @@
-#!/usr/bin/python
 import argparse
 import subprocess as sp
 import sys
 
-# %% Script statement
-"""
-    python brightness_control.py --resolution <parameter> --scale <parameter>
+class WLogout:
+    def __init__(self, resolution, scale):
+        self.margin = 400
+        self.resolution = resolution
+        self.scale = scale
+
+    def calculate_margin(self):
+        try:
+            self.resolution = int(self.resolution)
+            self.scale = int(self.scale)
+            self.margin = self.margin * 1080 * self.scale / self.resolution
+        except ValueError as e:
+            print(f"Invalid input: {e}")
+            sys.exit(1)
+
+    def execute_wlogout(self):
+        try:
+            self.calculate_margin()
+            sp.run(f"wlogout -b 6 -T {self.margin} -B {self.margin}", shell=True)
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            sys.exit(1)
+
+def main():
+    """
+    python wlogout.py --resolution <parameter> --scale <parameter>
         << --resolution >>
             resolution: integer as an example 1080
         << --scale >>
             scale: integer value as an example 1
-"""
+    """
 
-margin = 400
+    parser = argparse.ArgumentParser(description="Run wlogout with specified resolution and scale.")
+    parser.add_argument("-r", "--resolution", help="Monitor resolution as integer, ex. 1080", required=True)
+    parser.add_argument("-s", "--scale", help="Monitor scale as integer, ex. 1", required=True)
 
-# %% Argument
-parser = argparse.ArgumentParser()
-parser.add_argument("-r", "--resolution", help="monitor resolution as integer, ex. 1080")
-parser.add_argument("-s", "--scale", help="monitor resolution as integer, ex. 1")
+    args = parser.parse_args()
 
-args = parser.parse_args()
-resolution = args.resolution
-scale = args.scale
+    wlogout = WLogout(args.resolution, args.scale)
+    wlogout.execute_wlogout()
 
-#%% Wlogout
-try:
-    resolution = int(resolution)
-    scale = int(scale)
-    margin = margin * 1080 * scale/resolution
-    sp.run(f"""wlogout -b 6 -T {margin} -B {margin}""", shell=True)
-except Exception as e:
-    print(e)
-    sys.exit()
-
+if __name__ == "__main__":
+    main()
