2baf9b07f4722a52ffe2238a3a9ef40028b7ed09
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-08-14 23:37:47 +0300

    minor fixes

--- 07 - get_themes_icons_cursors.sh
+++ 07 - get_themes_icons_cursors.sh
@@ -3,77 +3,13 @@
 #------------#
 # GTK Themes #
 #------------#
-tar -xzvf .themes/Gtk_CatppuccinLatte.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_CatppuccinMocha.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_CyberpunkEdge.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_DecayGreen.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_Dracula.tar.xz -C ~/.themes
-tar -xzvf .themes/Gtk_FrostedGlass.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_GraphiteMono.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_GruvboxRetro.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_MaterialSakura.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_NordicBlue.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_RosePine.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_SweetMars.tar.gz -C ~/.themes
-tar -xzvf .themes/Gtk_SynthWave.tar.xz -C ~/.themes
+tar -xzvf .themes/GTK_themes.tar.gz -C ~/.themes
 
-sudo tar -xzvf .themes/Gtk_CatppuccinLatte.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_CatppuccinMocha.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_CyberpunkEdge.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_DecayGreen.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_Dracula.tar.xz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_FrostedGlass.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_GraphiteMono.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_GruvboxRetro.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_MaterialSakura.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_NordicBlue.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_RosePine.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_SweetMars.tar.gz -C /usr/share/themes
-sudo tar -xzvf .themes/Gtk_SynthWave.tar.xz -C /usr/share/themes
+sudo tar -xzvf .themes/GTK_themes.tar.gz -C /usr/share/themes
 
-#-------------#
-# Icon Themes #
-#-------------#
-tar -xzvf .icons/Cursor_Bibata-Modern-Classic.tar.xz -C ~/.icons
-tar -xzvf .icons/Cursor_BibataIce.tar.gz -C ~/.icons
-tar -xzvf .icons/Cursor_Dracula-cursors.tar.xz -C ~/.icons
-tar -xzvf .icons/Cursor_Gruvbox.tar.gz -C ~/.icons
-tar -xzvf .icons/Cursor_Sweet.tar.xz -C ~/.icons
+#----------------------#
+# Icon & Cursor Themes #
+#----------------------#
+tar -xzvf .themes/Icon_Cursor_themes.tar.gz -C ~/.icons
 
-sudo tar -xzvf .icons/Cursor_Bibata-Modern-Classic.tar.xz -C /usr/share/icons
-sudo tar -xzvf .icons/Cursor_BibataIce.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Cursor_Dracula-cursors.tar.xz -C /usr/share/icons
-sudo tar -xzvf .icons/Cursor_Gruvbox.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Cursor_Sweet.tar.xz -C /usr/share/icons
-
-#---------------#
-# Cursor Themes #
-#---------------#
-tar -xzvf .icons/Icon_candy-icons.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_Gruvbox.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_NordicBlue.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_SynthWave.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaBlack.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaBlue.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaDracula.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaGreen.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaGrey.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaPink.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaPurple.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_TelaYellow.tar.gz -C ~/.icons
-tar -xzvf .icons/Icon_Tela-red.tar.gz -C ~/.icons
-
-sudo tar -xzvf .icons/Icon_candy-icons.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_Gruvbox.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_NordicBlue.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_SynthWave.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaBlack.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaBlue.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaDracula.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaGreen.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaGrey.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaPink.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaPurple.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_TelaYellow.tar.gz -C /usr/share/icons
-sudo tar -xzvf .icons/Icon_Tela-red.tar.gz -C /usr/share/icons
-
+sudo tar -xzvf .icons/Icon_Cursor_themes.tar.gz -C /usr/share/icons
