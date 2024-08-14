e2541a3a9bb556d3a2d064fe6dd071e6951683e5
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-08-14 23:45:27 +0300

    minor fixes

--- 07 - get_themes_icons_cursors.sh
+++ 07 - get_themes_icons_cursors.sh
@@ -3,13 +3,13 @@
 #------------#
 # GTK Themes #
 #------------#
-tar -xzvf .themes/GTK_themes.tar.gz -C ~/.themes
+tar -xzvf .themes/GTK_Themes.tar.gz -C ~/.themes
 
-sudo tar -xzvf .themes/GTK_themes.tar.gz -C /usr/share/themes
+sudo tar -xzvf .themes/GTK_Themes.tar.gz -C /usr/share/themes
 
 #----------------------#
 # Icon & Cursor Themes #
 #----------------------#
-tar -xzvf .themes/Icon_Cursor_themes.tar.gz -C ~/.icons
+tar -xzvf .themes/Icon_Cursor_Themes.tar.gz -C ~/.icons
 
-sudo tar -xzvf .icons/Icon_Cursor_themes.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Icon_Cursor_Themes.tar.gz -C /usr/share/icons
