e7b8e89cbcb42a7adcccca947f18e76a5c47b980
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-08-15 00:22:40 +0300

    minor fixes

--- 07 - get_themes_icons_cursors.sh
+++ 07 - get_themes_icons_cursors.sh
@@ -3,13 +3,78 @@
 #------------#
 # GTK Themes #
 #------------#
-tar -xzvf GTK_Themes.tar.gz -C ~/.themes
+tar -xzvf .theme/Catppuccin-Latte.tar.gz -C ~/.themes
+tar -xzvf .theme/Catppuccin-Mocha.tar.gz -C ~/.themes
+tar -xzvf .theme/Cyberpunk-Edge.tar.gz -C ~/.themes
+tar -xzvf .theme/Decay-Green.tar.gz -C ~/.themes
+tar -xzvf .theme/Dracula.tar.gz -C ~/.themes
+tar -xzvf .theme/Frosted-Glass.tar.gz -C ~/.themes
+tar -xzvf .theme/Graphite-Mono.tar.gz -C ~/.themes
+tar -xzvf .theme/Gruvbox-Retro.tar.gz -C ~/.themes
+tar -xzvf .theme/Gruvbox.tar.gz -C ~/.themes
+tar -xzvf .theme/Material-Red.tar.gz -C ~/.themes
+tar -xzvf .theme/Material-Sakura.tar.gz -C ~/.themes
+tar -xzvf .theme/Nordic-Blue.tar.gz -C ~/.themes
+tar -xzvf .theme/Rose-Pine.tar.gz -C ~/.themes
+tar -xzvf .theme/Sweet-Mars.tar.gz -C ~/.themes
+tar -xzvf .theme/Synth-Wave.tar.gz -C ~/.themes
+tar -xzvf .theme/Tokyo-Night.tar.gz -C ~/.themes
 
-sudo tar -xzvf GTK_Themes.tar.gz -C /usr/share/themes
+
+sudo tar -xzvf .theme/Catppuccin-Latte.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Catppuccin-Mocha.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Cyberpunk-Edge.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Decay-Green.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Dracula.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Frosted-Glass.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Graphite-Mono.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Gruvbox-Retro.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Gruvbox.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Material-Red.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Material-Sakura.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Nordic-Blue.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Rose-Pine.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Sweet-Mars.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Synth-Wave.tar.gz -C /usr/share/themes
+sudo tar -xzvf .theme/Tokyo-Night.tar.gz -C /usr/share/themes
 
 #----------------------#
 # Icon & Cursor Themes #
 #----------------------#
-tar -xzvf Icon_Cursor_Themes.tar.gz -C ~/.icons
+tar -xzvf .icons/Beauty-Line.tar.gz -C ~/.icons
+tar -xzvf .icons/Bibata-Modern-Classic.tar.gz -C ~/.icons
+tar -xzvf .icons/Bibata-Modern-Ice.tar.gz -C ~/.icons
+tar -xzvf .icons/Candy-Icons.tar.gz -C ~/.icons
+tar -xzvf .icons/Dracula-Cursors.tar.gz -C ~/.icons
+tar -xzvf .icons/Gruvbox-Blue.tar.gz -C ~/.icons
+tar -xzvf .icons/Gruvbox-Cursors.tar.gz -C ~/.icons
+tar -xzvf .icons/Nordzy.tar.gz -C ~/.icons
+tar -xzvf .icons/Sweet-Cursors.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Black.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Blue.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Dracula.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Green.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Grey.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Pink.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Purple.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Circle-Yellow.tar.gz -C ~/.icons
+tar -xzvf .icons/Tela-Red.tar.gz -C ~/.icons
 
-sudo tar -xzvf Icon_Cursor_Themes.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Beauty-Line.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Bibata-Modern-Classic.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Bibata-Modern-Ice.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Candy-Icons.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Dracula-Cursors.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Gruvbox-Blue.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Gruvbox-Cursors.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Nordzy.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Sweet-Cursors.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Black.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Blue.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Dracula.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Green.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Grey.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Pink.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Purple.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Circle-Yellow.tar.gz -C /usr/share/icons
+sudo tar -xzvf .icons/Tela-Red.tar.gz -C /usr/share/icons
