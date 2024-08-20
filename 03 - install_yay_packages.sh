205952f1a130ff4f5b03f0423c2d537bf55d3efa
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-08-20 19:58:23 +0300

    removed some apps

--- 03 - install_yay_packages.sh
+++ 03 - install_yay_packages.sh
@@ -4,10 +4,7 @@
 # Apps #
 #------#
 yay --needed --noconfirm -S vscodium-bin
-# yay --needed --noconfirm -S pycharm-community-edition
 yay --needed --noconfirm -S brave-bin
-# yay --needed --noconfirm -S librewolf-bin
-# yay --needed --noconfirm -S zoom
 
 #-----#
 # AUR #
@@ -18,7 +15,6 @@ yay --needed --noconfirm -S pikaur
 # Wallpaper #
 #-----------#
 yay --needed --noconfirm -S swww
-yay --needed --noconfirm -S waypaper
 
 #------------#
 # Screenshot #
@@ -29,11 +25,6 @@ yay --needed --noconfirm -S hyprshot
 # Logout Menu #
 #-------------#
 yay --needed --noconfirm -S wlogout
-
-#------------#
-# Screenlock #
-#------------#
-yay --needed --noconfirm -S hyprlock-git
 
 #------------------#
 # Application Menu #
@@ -54,14 +45,6 @@ yay --needed --noconfirm -S power-profiles-daemon
 # Theme / Icon #
 #--------------#
 yay --needed --noconfirm -S nwg-look
-yay --needed --noconfirm -S sddm-sugar-candy-git
-# yay --needed --noconfirm -S tela-icon-theme
-# yay --needed --noconfirm -S bibata-cursor-theme
-# yay --needed --noconfirm -S catppuccin-gtk-theme-mocha
-# yay --needed --noconfirm -S catppuccin-gtk-theme-latte
-# yay --needed --noconfirm -S catppuccin-gtk-theme-macchiato
-# yay --needed --noconfirm -S catppuccin-gtk-theme-frappe
-# yay --needed --noconfirm -S kvantum-theme-catppuccin-git
 
 #--------------#
 # Emoji Picker #
