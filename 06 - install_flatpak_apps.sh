0b8a2c6fbd92576513de85d146991ed0fbf22550
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-08-24 00:53:54 +0300

    update 06 - install_flatpak_apps.sh

--- 06 - install_flatpak_apps.sh
+++ 06 - install_flatpak_apps.sh
@@ -5,5 +5,6 @@
 #--------#
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub it.mijorus.collector
+flatpak install flathub io.github.shiftey.Desktop
 # flatpak install flathub net.pcsx2.PCSX2
 # flatpak install flathub org.ppsspp.PPSSPP
