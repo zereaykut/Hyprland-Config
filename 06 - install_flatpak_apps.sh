74c941a2f18828293fe2a7043a13c5d7f39fc38a
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-08-30 11:06:51 +0300

    update 06 - install_flatpak_apps.sh
    added new gui github client named turle

--- 06 - install_flatpak_apps.sh
+++ 06 - install_flatpak_apps.sh
@@ -5,6 +5,7 @@
 #--------#
 flatpak install flathub com.github.tchx84.Flatseal
 flatpak install flathub it.mijorus.collector
-flatpak install flathub io.github.shiftey.Desktop
+flatpak install flathub de.philippun1.turtle        # GUI client for GitHub named turtle
+# flatpak install flathub io.github.shiftey.Desktop   # Github Desktop
 # flatpak install flathub net.pcsx2.PCSX2
 # flatpak install flathub org.ppsspp.PPSSPP
