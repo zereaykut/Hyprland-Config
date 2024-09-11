0ea79900c2fcab92677f595981a6c289b9644c64
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-09-11 22:22:59 +0300

    Update 06 - install_flatpak_apps.sh
    added obs-studio

--- 06 - install_flatpak_apps.sh
+++ 06 - install_flatpak_apps.sh
@@ -4,10 +4,11 @@
 # Apps   #
 #--------#
 flatpak install flathub com.github.tchx84.Flatseal
+flatpak install flathub io.github.flattool.Warehouse  # Manage Flatpak Apps
 flatpak install flathub it.mijorus.collector
 flatpak install flathub de.philippun1.turtle         # GUI client for GitHub named turtle
 # flatpak install flathub io.github.shiftey.Desktop    # Github Desktop
-flatpak install flathub io.github.flattool.Warehouse  # Manage Flatpak Apps
 flatpak install flathub com.brave.Browser            # Brave Browser
+flatpak install flathub com.obsproject.Studio
 # flatpak install flathub net.pcsx2.PCSX2
 # flatpak install flathub org.ppsspp.PPSSPP
