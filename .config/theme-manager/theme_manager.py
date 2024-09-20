edbb5453a74476a0756c9ea65252f776d2344a3f
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-09-20 21:02:10 +0300

    update: theme_manager.py
    changed color schemes cache path

--- .config/theme-manager/theme_manager.py
+++ .config/theme-manager/theme_manager.py
@@ -45,7 +45,7 @@ class ThemeManager:
         rofi_select = sp.run(f"""echo -en "{rofi_output}" | rofi -dmenu -matching normal -i -theme ~/.config/rofi/wallpaper_select.rasi""", shell=True, capture_output=True, text=True)
         wallpaper = rofi_select.stdout.strip()
         color_scheme = wallpaper.replace(".png", "").replace(".jpg", "")
-        with open(f"/home/{self.user}/.cache/color_schemes/{theme}/{color_scheme}.json", "r") as f:
+        with open(f"/home/{self.user}/.cache/theme-manager/color-schemes/{theme}/{color_scheme}.json", "r") as f:
             color_config = json.load(f)
         return f"/home/{self.user}/.config/wallpapers/{theme}/{wallpaper}", color_config
 
