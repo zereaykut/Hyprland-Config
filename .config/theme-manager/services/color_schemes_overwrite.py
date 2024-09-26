e34e887ef63659477848e5cf0d72282e16a73506
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-09-26 22:20:42 +0300

    added: overwrite color schemes
    new script saves color schemes whether they are saved or not.

---
+++ .config/theme-manager/services/color_schemes_overwrite.py
@@ -0,0 +1,30 @@
+import subprocess as sp
+from color_schemes import ColorSchemeGenerator
+
+class ColorSchemeGeneratorOW(ColorSchemeGenerator):
+    def process_images(self):
+        """
+        Process each image and generate a color scheme.
+        Overwrite existing color schemes.
+        """
+        images_info = self.get_images()
+
+        for image_info in images_info:
+            print(image_info[0])
+            
+            try:
+                self.get_color_scheme(image_info[0], image_info[1], image_info[2])
+            except Exception as e:
+                error_path = f"{self.base_dir}/{image_info[1]}/{image_info[2]}.error"
+                with open(error_path, "w") as f:
+                    f.writelines("Error:\n")
+                    f.writelines(f"{e}:\n")
+                    f.writelines(f"{image_info[0]}\n")
+                    f.writelines(f"{image_info[1]}\n")
+                    f.writelines(f"{image_info[2]}\n")
+
+
+if __name__ == "__main__":
+    user = sp.run("whoami", shell=True, capture_output=True, text=True).stdout.strip()
+    generator = ColorSchemeGeneratorOW(user)
+    generator.process_images()
