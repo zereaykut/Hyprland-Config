2d2c7d9f6dbac92bf1a44835c2f12c30f810ef1d
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-10-18 22:43:12 +0300

    upload: battery notify

---
+++ .config/hypr/scripts/battery_notify.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+
+import subprocess as sp
+import time
+
+import psutil
+
+BATTERY_CRITICAL = 20
+BATTERY_URGENT = 30
+
+
+def secs2hours(secs: int) -> str:
+    mm, ss = divmod(secs, 60)
+    hh, mm = divmod(mm, 60)
+    return "%d:%02d:%02d" % (hh, mm, ss)
+
+
+def get_battery_info() -> dict:
+    battery_info = psutil.sensors_battery()
+    return {
+        "percent": round(battery_info.percent, 2),
+        "secsleft": secs2hours(battery_info.secsleft),
+        "power_plugged": battery_info.power_plugged,
+    }
+
+
+def send_notification(message: str, icon: str, timeout: int = 800) -> None:
+    sp.run(
+        f"""dunstify "{message}" -i {icon} -r 92999 -t {timeout} -u normal""",
+        shell=True,
+    )
+
+
+if __name__ == "__main__":
+    user = sp.run("whoami", shell=True, capture_output=True, text=True).stdout.strip()
+    while True:
+        battery_info = get_battery_info()
+        if battery_info.percent <= BATTERY_URGENT:
+            send_notification(
+                f"""Percentage: {battery_info["percent"]}%\nPlugged:{battery_info["power_plugged"]}""",
+                f"/home/{user}/.config/dunst/icons/status/battery-status.png",
+                10000,
+            )
+        elif battery_info.percent <= BATTERY_CRITICAL:
+            send_notification(
+                f"""Percentage: {battery_info["percent"]}%\nPlugged:{battery_info["power_plugged"]}""",
+                f"/home/{user}/.config/dunst/icons/status/battery-status.png",
+                10000,
+            )
+
+        time.sleep(30)
