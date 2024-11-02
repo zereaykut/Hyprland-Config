1dd293454e3a81ee4d12413ac512483316c79814
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-11-03 02:23:05 +0300

    update: clip_manager

--- .local/share/bin/clip_manager.py
+++ .local/share/bin/clip_manager.py
@@ -6,69 +6,37 @@ import subprocess as sp
 class ClipManager:
     def __init__(self):
         self.user = sp.run("whoami", shell=True, capture_output=True, text=True).stdout.strip()
-        self.modes = ["History", "Favorites", "Delete", "Clear History", "Exit"]
 
     def mode(self):
-        return sp.run( f"""echo "{"\n".join(self.modes)}" | rofi -dmenu -matching normal -i""", shell=True, capture_output=True, text=True).stdout.strip()
+        return sp.run("""cliphist list | rofi -i -dmenu -kb-custom-1 "Control-Delete" -kb-custom-2 "Alt-Delete" """, shell=True, capture_output=True, text=True)
 
     def notify(self, message: str, icon: str, timeout: int = 800) -> None:
-        sp.run(
-            f"""notify-send "{message}" -i {icon} -r 92999 -t {timeout} -u normal""",
-            shell=True,
-        )
-
-    def history(self):
-        output = sp.run("cliphist list | rofi -dmenu | cliphist decode | wl-copy", shell=True, capture_output=True, text=True).stdout
-        print(output)
-        self.notify(
-            output,
-            f"/home/{self.user}/.config/dunst/icons/status/clip.svg",
-            800
-        )
-        sys.exit()
-
-    def favorites(self):
-        pass
-    
-    def delete(self):
-        output = sp.run("cliphist list | rofi -dmenu | cliphist delete", shell=True, capture_output=True, text=True).stdout
-        self.notify(
-            output,
-            f"/home/{self.user}/.config/dunst/icons/status/clip-edit.svg",
-            800
-        )
-
-    def claer_history(self):
-        sp.run("cliphist wipe", shell=True)
-        self.notify(
-            "Clipboard History Cleared",
-            f"/home/{self.user}/.config/dunst/icons/status/clip-edit.svg",
-            800
-        )
-    
-    def exit(self):
-        sys.exit()
+        sp.run(f"""notify-send "{message}" -i {icon} -r 92999 -t {timeout} -u normal""", shell=True)
 
     def run(self):
-        # parser = argparse.ArgumentParser()
-        # parser.add_argument("-a", "--mode", help="Clip manager modes among History, Favorites, Delete, Clear History, and Exit")
+        output = self.mode()
+        returncode = output.returncode
+        stdout = output.stdout
+        print(returncode)
+        print(stdout)
+        if returncode == 10:
+            # CTRL-Delete: 10
+            print("Delete")
+            sp.run(f'cliphist delete <<<"{stdout}"', shell=True)
+            self.notify(stdout, f"/home/{self.user}/.config/dunst/icons/status/clip-edit.svg", 1000)
+        elif returncode == 11:
+            # ALT-Delete: 11
+            print("Clear")
+            sp.run("cliphist wipe", shell=True)
+            self.notify("Clipbboard History Cleared", f"/home/{self.user}/.config/dunst/icons/status/clip-edit.svg", 1000)
+        elif returncode == 0:
+            # Enter or Selection: 0
+            print("Select")
+            stdout = stdout.split("\t", 1)[1].strip()
+            sp.run(f'wl-copy "{stdout}"', shell=True)
+            self.notify(stdout, f"/home/{self.user}/.config/dunst/icons/status/clip.svg", 1000)
 
-        # args = parser.parse_args()
-        # mode_ = args.mode
-        mode_ = self.mode()
-
-        if mode_ == "History":
-            self.history()
-        elif mode_ == "Favorites":
-            sys.exit()
-        elif mode_ == "Delete":
-            while True:
-                self.delete()
-        elif mode_ == "Clear History":
-            self.claer_history()
-        else:
-            print("No arguments")
-            sys.exit()
+            
 
 if __name__ == "__main__":
     # main()
