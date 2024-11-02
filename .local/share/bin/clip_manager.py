f14821831bedc192da75a1e8266d7637075fcbc4
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-11-02 23:45:42 +0300

    update: clip_manager

--- .local/share/bin/clip_manager.py
+++ .local/share/bin/clip_manager.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
+import argparse
 import sys
 import subprocess as sp
 
 class ClipManager:
     def __init__(self):
         self.user = sp.run("whoami", shell=True, capture_output=True, text=True).stdout.strip()
+        self.modes = ["History", "Favorites", "Delete", "Clear History", "Exit"]
 
     def mode(self):
-        modes = ["History", "Favorites", "Delete", "Clear History", "Exit"]
-        return sp.run( f"""echo "{"\n".join(modes)}" | rofi -dmenu -matching normal -i""", shell=True, capture_output=True, text=True).stdout.strip()
+        return sp.run( f"""echo "{"\n".join(self.modes)}" | rofi -dmenu -matching normal -i""", shell=True, capture_output=True, text=True).stdout.strip()
 
     def notify(self, message: str, icon: str, timeout: int = 800) -> None:
         sp.run(
@@ -16,14 +17,15 @@ class ClipManager:
             shell=True,
         )
 
-
     def history(self):
         output = sp.run("cliphist list | rofi -dmenu | cliphist decode | wl-copy", shell=True, capture_output=True, text=True).stdout
+        print(output)
         self.notify(
             output,
             f"/home/{self.user}/.config/dunst/icons/status/clip.svg",
             800
         )
+        sys.exit()
 
     def favorites(self):
         pass
@@ -43,26 +45,33 @@ class ClipManager:
             f"/home/{self.user}/.config/dunst/icons/status/clip-edit.svg",
             800
         )
+    
+    def exit(self):
+        sys.exit()
 
+    def run(self):
+        # parser = argparse.ArgumentParser()
+        # parser.add_argument("-a", "--mode", help="Clip manager modes among History, Favorites, Delete, Clear History, and Exit")
 
+        # args = parser.parse_args()
+        # mode_ = args.mode
+        mode_ = self.mode()
 
-def main():
-    # while True:
-    cliphist_list = sp.run("cliphist list", shell=True, capture_output=True, text=True).stdout#.splitlines()
-    # cliphist_list = [val + "\n" for val in cliphist_list]
-    # print(cliphist_list)
-    cmd = [
-        "rofi", "-i", "-dmenu", "-kb-custom-1", "Control-Delete", 
-        "-kb-custom-2", "Alt-Delete",
-        "-config", "~/.config/rofi/config.rasi",
-        ]
-    print(cmd)
-    output = sp.run(f"""rofi -i -dmenu -kb-custom-1 "Control-Delete" -kb-custom-2 "Alt-Delete" -config ~/.config/rofi/config.rasi""", shell=True, capture_output=True, text=True, check=False).stdout
-    # output = sp.run(cmd, capture_output=True, check=False).stdout
-    # output = sp.check_output(cmd)
-    print(output)
+        if mode_ == "History":
+            self.history()
+        elif mode_ == "Favorites":
+            sys.exit()
+        elif mode_ == "Delete":
+            while True:
+                self.delete()
+        elif mode_ == "Clear History":
+            self.claer_history()
+        else:
+            print("No arguments")
+            sys.exit()
 
 if __name__ == "__main__":
     # main()
     clip_manager = ClipManager()
-    print(clip_manager.mode())
+    # print(clip_manager.mode())
+    clip_manager.run()
