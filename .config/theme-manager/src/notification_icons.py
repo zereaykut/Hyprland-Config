e948fb2486bac0f5895c64f19389d025d7ecc253
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-11-05 23:39:37 +0300

    update: notification icons

--- .config/theme-manager/src/notification_icons.py
+++ .config/theme-manager/src/notification_icons.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python
 
 
-def status_icons(color_config: dict, status_icons_list: list[str], user: str) -> None:
+def status_icons(color_config: dict, status_icons_list: list[str], home: str, notify_daemon: str) -> None:
     for icon in status_icons_list:
-        with open(
-            f"/home/{user}/.config/theme-manager/icons/status/{icon}.svg",
-            "r",
-            encoding="UTF-8",
-        ) as f:
+        with open(f"{home}/.config/theme-manager/icons/status/{icon}.svg", "r", encoding="UTF-8") as f:
             data = f.read()
-        print(color_config)
         data = data.replace("""{{wb-hvr-bg}}""", color_config["wb-hvr-bg"])
 
-        with open(
-            f"/home/{user}/.config/dunst/icons/status/{icon}.svg", "w", encoding="UTF-8"
-        ) as file:
+        with open(f"{home}/.config/{notify_daemon}/icons/status/{icon}.svg", "w", encoding="UTF-8") as file:
             file.write(data)
 
 
-def vol_icons(color_config: dict, user: str) -> None:
+def vol_icons(color_config: dict, home: str, notify_daemon: str) -> None:
     for vol in range(0, 101, 5):
-        with open(
-            f"/home/{user}/.config/theme-manager/icons/vol/vol-{vol}.svg",
-            "r",
-            encoding="UTF-8",
-        ) as file:
+        with open(f"{home}/.config/theme-manager/icons/vol/vol-{vol}.svg", "r", encoding="UTF-8") as file:
             data = file.read()
 
         data = data.replace("""{{wb-hvr-fg}}""", color_config["wb-hvr-fg"])
         data = data.replace("""{{wb-hvr-bg}}""", color_config["wb-hvr-bg"])
 
-        with open(
-            f"/home/{user}/.config/dunst/icons/vol/vol-{vol}.svg", "w", encoding="UTF-8"
-        ) as file:
+        with open(f"{home}/.config/{notify_daemon}/icons/vol/vol-{vol}.svg", "w", encoding="UTF-8") as file:
             file.write(data)
 
