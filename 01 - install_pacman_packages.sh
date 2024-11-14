692aa462813c2e1e07e3054b2934f827bf7d0f6e
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2024-11-14 18:48:49 +0300

    update: 01 - install_pacman_packages.sh
    added python-pillow for theme-manager

--- 01 - install_pacman_packages.sh
+++ 01 - install_pacman_packages.sh
@@ -13,6 +13,7 @@ sudo pacman --needed --noconfirm -S base-devel
 sudo pacman --needed --noconfirm -S python
 sudo pacman --needed --noconfirm -S python-pip
 sudo pacman --needed --noconfirm -S python-psutil
+sudo pacman --needed --noconfirm -S python-pillow
 # sudo pacman --needed --noconfirm -S python-rich
 # sudo pacman --needed --noconfirm -S python-click
 # sudo pacman --needed --noconfirm -S python-requests
