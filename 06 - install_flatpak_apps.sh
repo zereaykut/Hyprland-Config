f336a29a323c0723ab60a0dc0012afe4ebaa27e1
Aykut Zere <102432357+zereaykut@users.noreply.github.com>
2025-01-12 17:26:04 +0300

    update: 06 - install_flatpak_apps.sh

--- 06 - install_flatpak_apps.sh
+++ 06 - install_flatpak_apps.sh
@@ -3,14 +3,15 @@
 #--------#
 # Apps   #
 #--------#
-flatpak install -y --noninteractive flathub com.github.tchx84.Flatseal      # Manage Flatpak permissions
+flatpak install -y --noninteractive flathub com.github.tchx84.Flatseal       # Manage Flatpak permissions
 flatpak install -y --noninteractive flathub io.github.flattool.Warehouse     # Manage Flatpak Apps
-flatpak install -y --noninteractive flathub it.mijorus.collector            # Drag and Drop files
-# flatpak install -y --noninteractive flathub io.github.shiftey.Desktop      # Github Desktop
-flatpak install -y --noninteractive flathub com.brave.Browser               # Brave Browser
-flatpak install -y --noninteractive flathub com.obsproject.Studio           # OBS
-# flatpak install -y --noninteractive flathub net.pcsx2.PCSX2                # PS2 Emulator
-# flatpak install -y --noninteractive flathub org.ppsspp.PPSSPP              # PSP Emulator
-# flatpak install flathub io.gpt4all.gpt4all                                 # Qt based GUI for GPT4All
-# flatpak install flathub com.jeffser.Alpaca                                  # An Ollama client
-flatpak install flathub me.proton.Mail                                      # Proton Mail
+flatpak install -y --noninteractive flathub com.brave.Browser                # Brave Browser
+flatpak install -y --noninteractive flathub io.missioncenter.MissionCenter   # system monitor
+flatpak install -y --noninteractive flathub it.mijorus.collector             # Drag and Drop files
+# flatpak install -y --noninteractive flathub io.github.shiftey.Desktop        # Github Desktop
+flatpak install -y --noninteractive flathub com.obsproject.Studio            # OBS
+# flatpak install -y --noninteractive flathub net.pcsx2.PCSX2                  # PS2 Emulator
+# flatpak install -y --noninteractive flathub org.ppsspp.PPSSPP                # PSP Emulator
+# flatpak install flathub io.gpt4all.gpt4all                                   # Qt based GUI for GPT4All
+# flatpak install flathub com.jeffser.Alpaca                                   # An Ollama client
+# flatpak install -y --noninteractive flathub me.proton.Mail                   # Proton Mail
