3c6992936d77d3c2db8a750180fb11b8d84d080f
Aykut Zere <halil.aykut.zere@gmail.com>
2025-03-18 10:37:44 +0300

    updates

---
+++ Configs/.config/hyprdots/themes/Catppuccin-Frappe/variables.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export GTK_THEME="Catppuccin-Frappe"
+export ICON_THEME="Tela-Circle-Pink"
+export CURSOR_THEME="Bibata-Modern-Classic"
+export CURSOR_SIZE=24
+export COLOR_SCHEME="prefer-dark"
