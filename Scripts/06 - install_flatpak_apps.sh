51c76b9ff8f5d14b0cd7d6c8a78c4a4509e95b53
Aykut Zere <halil.aykut.zere@gmail.com>
2025-06-24 20:45:30 +0300

    update: flatpak apps

--- Scripts/06 - install_flatpak_apps.sh
+++ Scripts/06 - install_flatpak_apps.sh
@@ -9,6 +9,7 @@ flatpak install -y --noninteractive flathub it.mijorus.collector             # d
 # flatpak install -y --noninteractive flathub io.github.shiftey.Desktop        # github desktop
 flatpak install -y --noninteractive flathub com.obsproject.Studio            # OBS
 flatpak install -y --noninteractive flathub io.podman_desktop.PodmanDesktop  # podman desktop
+flatpak install -y --noninteractive flathub org.localsend.localsend_app      # localsend
 # flatpak install -y --noninteractive flathub net.pcsx2.PCSX2                  # PS2 emulator
 # flatpak install -y --noninteractive flathub org.ppsspp.PPSSPP                # PSP emulator
 # flatpak install flathub io.gpt4all.gpt4all                                   # Qt based GUI for GPT4All
